589d9939f7494de3953ae98065a783b479486fb9
Mikhail <mikes22@yandex.ru>
2020-11-03 16:16:23 +0300

    Add Lab05

---
+++ IntelSysLab05/IntelSysLab05/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace IntelSysLab05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FactsList = new System.Windows.Forms.CheckedListBox();
+            this.FwdSel = new System.Windows.Forms.RadioButton();
+            this.BkwdSel = new System.Windows.Forms.RadioButton();
+            this.BkwdResFactsList = new System.Windows.Forms.ComboBox();
+            this.LogBox = new System.Windows.Forms.RichTextBox();
+            this.GoBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FactsList
+            // 
+            this.FactsList.FormattingEnabled = true;
+            this.FactsList.Location = new System.Drawing.Point(13, 13);
+            this.FactsList.Name = "FactsList";
+            this.FactsList.Size = new System.Drawing.Size(344, 424);
+            this.FactsList.TabIndex = 0;
+            // 
+            // FwdSel
+            // 
+            this.FwdSel.AutoSize = true;
+            this.FwdSel.Checked = true;
+            this.FwdSel.Location = new System.Drawing.Point(444, 12);
+            this.FwdSel.Name = "FwdSel";
+            this.FwdSel.Size = new System.Drawing.Size(65, 17);
+            this.FwdSel.TabIndex = 1;
+            this.FwdSel.TabStop = true;
+            this.FwdSel.Text = "Прямой";
+            this.FwdSel.UseVisualStyleBackColor = true;
+            this.FwdSel.Click += new System.EventHandler(this.FwdSel_Click);
+            // 
+            // BkwdSel
+            // 
+            this.BkwdSel.AutoSize = true;
+            this.BkwdSel.Location = new System.Drawing.Point(444, 36);
+            this.BkwdSel.Name = "BkwdSel";
+            this.BkwdSel.Size = new System.Drawing.Size(76, 17);
+            this.BkwdSel.TabIndex = 2;
+            this.BkwdSel.Text = "Обратный";
+            this.BkwdSel.UseVisualStyleBackColor = true;
+            this.BkwdSel.Click += new System.EventHandler(this.BkwdSel_Click);
+            // 
+            // BkwdResFactsList
+            // 
+            this.BkwdResFactsList.Enabled = false;
+            this.BkwdResFactsList.FormattingEnabled = true;
+            this.BkwdResFactsList.Location = new System.Drawing.Point(444, 59);
+            this.BkwdResFactsList.Name = "BkwdResFactsList";
+            this.BkwdResFactsList.Size = new System.Drawing.Size(343, 21);
+            this.BkwdResFactsList.TabIndex = 3;
+            // 
+            // LogBox
+            // 
+            this.LogBox.Location = new System.Drawing.Point(444, 88);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(344, 350);
+            this.LogBox.TabIndex = 4;
+            this.LogBox.Text = "";
+            // 
+            // GoBtn
+            // 
+            this.GoBtn.Location = new System.Drawing.Point(364, 211);
+            this.GoBtn.Name = "GoBtn";
+            this.GoBtn.Size = new System.Drawing.Size(75, 23);
+            this.GoBtn.TabIndex = 5;
+            this.GoBtn.Text = "->";
+            this.GoBtn.UseVisualStyleBackColor = true;
+            this.GoBtn.Click += new System.EventHandler(this.GoBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GoBtn);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.BkwdResFactsList);
+            this.Controls.Add(this.BkwdSel);
+            this.Controls.Add(this.FwdSel);
+            this.Controls.Add(this.FactsList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox FactsList;
+        private System.Windows.Forms.RadioButton FwdSel;
+        private System.Windows.Forms.RadioButton BkwdSel;
+        private System.Windows.Forms.ComboBox BkwdResFactsList;
+        private System.Windows.Forms.RichTextBox LogBox;
+        private System.Windows.Forms.Button GoBtn;
+    }
+}
+
---
+++ IntelSysLab05/IntelSysLab05/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IntelSysLab05
+{
+    public partial class Form1 : Form
+    {
+        //num, type, fact
+        IEnumerable<(int, char, string)> facts;
+        IEnumerable<(IEnumerable<int>, int)> rules;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadDB();
+            foreach(var f in facts.Where(f => f.Item2 == '*'))
+            {
+                FactsList.Items.Add(f);
+            }
+            foreach (var f in facts.Where(f => f.Item2 == '/'))
+            {
+                BkwdResFactsList.Items.Add(f);
+            }
+            BkwdResFactsList.SelectedIndex = 0;
+        }
+
+        private (int, char, string) ParseFact(string line)
+        {
+            var t = line.Trim().Split(' ');
+            if (!(t[1] == "*" || t[1] == "/"))
+                return (int.Parse(t[0]), ' ', string.Join(" ", t.Skip(1)));
+            else
+                return (int.Parse(t[0]), t[1][0], string.Join(" ", t.Skip(2)));
+        }
+
+        private (IEnumerable<int>, int) ParseRule(string line)
+        {
+            var t = line.Trim().Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
+            return (t.First().Split(',').Select(s => int.Parse(s.Trim())), int.Parse(t.Last().Trim()));
+
+        }
+
+        public void ReadDB()
+        {
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            facts = File.ReadLines(path + "/../../facts.txt").Select(line => ParseFact(line));
+
+            rules = File.ReadLines(path + "/../../rules.txt").Select(line => ParseRule(line));
+        }
+
+        private void BkwdSel_Click(object sender, EventArgs e)
+        {
+            FwdSel.Checked = !BkwdSel.Checked;
+            BkwdResFactsList.Enabled = true;
+        }
+
+        private void FwdSel_Click(object sender, EventArgs e)
+        {
+            BkwdSel.Checked = !FwdSel.Checked;
+            BkwdResFactsList.Enabled = false;
+        }
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+            LogBox.Clear();
+            if(FwdSel.Checked)
+                Go(FactsList.CheckedItems.Cast<(int, char, string)>());
+            else
+                Go(FactsList.CheckedItems.Cast<(int, char, string)>(), 1, ((int, char, string))BkwdResFactsList.SelectedItem);
+        }
+
+        private void Go(IEnumerable<(int, char, string)> inFacts, int Bkwd=0, (int, char, string) res = default((int, char, string)))
+        {
+            var cur = new HashSet< (int, char, string)>(inFacts);
+            var curNums = new HashSet<int>(cur.Select(t => t.Item1));
+            List<(int, char, string)> prev;
+            int fin;
+            do
+            {
+                prev = cur.ToList();
+                foreach (var r in rules)
+                {
+                    if (r.Item1.All(curNums.Contains) && !curNums.Contains(r.Item2))//Все требования правила выполнены
+                    {
+                        var ttt = facts.Where(f => f.Item1 == r.Item2).First();
+                        cur.Add(ttt);
+                        curNums.Add(ttt.Item1);
+                        LogBox.Text += "Added fact:" + ttt.ToString()+ " by ("+string.Join(",", r.Item1)+") -> "+ r.Item2  + "\n";
+                    }
+                }
+                fin = cur.Where(f => f.Item2 == '/').Count();
+            }
+            while ((Bkwd == 0 ? fin == 0 : !cur.Contains(res)) && cur.Count() > prev.Count());
+            if (Bkwd == 0)
+            {
+                if (fin > 0)
+                    LogBox.Text += "Result: " + string.Join(", ", cur.Where(f => f.Item2 == '/'));
+                else
+                    LogBox.Text += "Result: " + "Подходящих заведений не найдено.";
+            }
+            else
+            {
+                if (cur.Contains(res))
+                    LogBox.Text += "Result: " + res.ToString() + " reached.";
+                else
+                    LogBox.Text += "Result: " + res.ToString() + " not reached.";
+            }
+        }
+    }
+}
