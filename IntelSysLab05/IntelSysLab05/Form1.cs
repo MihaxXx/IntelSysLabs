85a217ad81336623514c4e4ae2a9dfdde8295bcf
Mikhail <mikes22@yandex.ru>
2020-11-04 20:19:19 +0000

    Add Bkwd

--- IntelSysLab05/IntelSysLab05/Form1.cs
+++ IntelSysLab05/IntelSysLab05/Form1.cs
@@ -14,8 +14,8 @@ namespace IntelSysLab05
     public partial class Form1 : Form
     {
         //num, type, fact
-        IEnumerable<(int, char, string)> facts;
-        IEnumerable<(IEnumerable<int>, int)> rules;
+        List<(int, char, string)> facts;
+        List<(List<int>, int)> rules;
 
         public Form1()
         {
@@ -41,10 +41,10 @@ namespace IntelSysLab05
                 return (int.Parse(t[0]), t[1][0], string.Join(" ", t.Skip(2)));
         }
 
-        private (IEnumerable<int>, int) ParseRule(string line)
+        private (List<int>, int) ParseRule(string line)
         {
             var t = line.Trim().Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
-            return (t.First().Split(',').Select(s => int.Parse(s.Trim())), int.Parse(t.Last().Trim()));
+            return (t.First().Split(',').Select(s => int.Parse(s.Trim())).ToList(), int.Parse(t.Last().Trim()));
 
         }
 
@@ -52,9 +52,9 @@ namespace IntelSysLab05
         {
             var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
-            facts = File.ReadLines(path + "/../../facts.txt").Select(line => ParseFact(line));
+            facts = File.ReadLines(path + "/../../facts.txt").Select(line => ParseFact(line)).ToList();
 
-            rules = File.ReadLines(path + "/../../rules.txt").Select(line => ParseRule(line));
+            rules = File.ReadLines(path + "/../../rules.txt").Select(line => ParseRule(line)).ToList();
         }
 
         private void BkwdSel_Click(object sender, EventArgs e)
@@ -72,10 +72,17 @@ namespace IntelSysLab05
         private void GoBtn_Click(object sender, EventArgs e)
         {
             LogBox.Clear();
-            if(FwdSel.Checked)
+            if (FwdSel.Checked)
                 Go(FactsList.CheckedItems.Cast<(int, char, string)>());
             else
-                Go(FactsList.CheckedItems.Cast<(int, char, string)>(), 1, ((int, char, string))BkwdResFactsList.SelectedItem);
+            {
+                var res = Backward(FactsList.CheckedItems.Cast<(int, char, string)>().ToList(), ((int, char, string))BkwdResFactsList.SelectedItem, new HashSet<int>());
+                if (res)
+                    LogBox.Text += "Result: " + (((int, char, string))BkwdResFactsList.SelectedItem).ToString() + " reached.";
+                else
+                    LogBox.Text += "Result: " + (((int, char, string))BkwdResFactsList.SelectedItem).ToString()+ " not reached.";
+                //Go(FactsList.CheckedItems.Cast<(int, char, string)>(), 1, ((int, char, string))BkwdResFactsList.SelectedItem);
+            }
         }
 
         private void Go(IEnumerable<(int, char, string)> inFacts, int Bkwd=0, (int, char, string) res = default((int, char, string)))
@@ -114,6 +121,56 @@ namespace IntelSysLab05
                 else
                     LogBox.Text += "Result: " + res.ToString() + " not reached.";
             }
+        }
+
+        public bool Backward(List<(int, char, string)> knowledge, (int, char, string) factToProve, HashSet<int> openFacts, int depth = 1)
+        {
+            depth++;
+
+            // если факт в базе знаний
+            if (knowledge.Contains(factToProve))
+            {
+                return true;
+            }
+
+            // если мы этот факт уже пытались доказать
+            if (openFacts.Contains(factToProve.Item1))
+            {
+                return false;
+            }
+            else
+                openFacts.Add(factToProve.Item1);
+
+            // ищем все продукции, в которых наш факт явлется следствием
+            var AcceptRules = rules.Where(r => r.Item2 == factToProve.Item1).ToList();
+
+            // если подходящих продукций нет
+            if (AcceptRules.Count == 0)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < AcceptRules.Count; i++)
+            {
+                bool f = true;
+
+                foreach (var cond in AcceptRules[i].Item1)
+                {
+                    if (!Backward(knowledge, facts.Where(fct => fct.Item1 == cond).First(), openFacts, depth))
+                    {
+                        f = false;
+                    }
+                }
+
+                if (f)
+                {
+                    knowledge.Add(factToProve);
+                    LogBox.Text += "Added fact:" + factToProve.ToString() + " by (" + string.Join(",", AcceptRules[i].Item1) + ") -> " + AcceptRules[i].Item2 + "\n";
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
