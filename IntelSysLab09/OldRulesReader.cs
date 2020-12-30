b93f9db869fb73e29cbb4bcb6f5a4c1a818fead8
Mikhail <mikes22@yandex.ru>
2020-12-30 15:18:21 +0300

    Add Lab09 (derived from Lab06)

---
+++ IntelSysLab09/OldRulesReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClipsFormsExample
+{
+    static class OldRulesReader
+    {
+        static private(int, char, string) ParseFact(string line)
+        {
+            var t = line.Trim().Split(' ');
+            if (!(t[1] == "*" || t[1] == "/"))
+                return (int.Parse(t[0]), ' ', string.Join(" ", t.Skip(1)));
+            else
+                return (int.Parse(t[0]), t[1][0], string.Join(" ", t.Skip(2)));
+        }
+
+        static private (List<int>, int) ParseRule(string line)
+        {
+            var t = line.Trim().Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
+            return (t.First().Split(',').Select(s => int.Parse(s.Trim())).ToList(), int.Parse(t.Last().Trim()));
+
+        }
+
+        static public List<(int, char, string)> ReadFacts()
+        {
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (!File.Exists(path + "/../../facts.txt"))
+                return null;
+            return File.ReadLines(path + "/../../facts.txt").Select(line => ParseFact(line)).ToList();
+        }
+        static public List<(List<int>, int)> ReadRules()
+        {
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (!File.Exists(path + "/../../rules.txt"))
+                return null;
+            return File.ReadLines(path + "/../../rules.txt").Select(line => ParseRule(line)).ToList();
+        }
+        static public string ConvertRulesToClips(List<(int, char, string)> facts, List<(List<int>, int)> rules)
+        {
+            string res = string.Empty;
+            foreach(var rule in rules.Select(r => (r.Item1.Select(req => facts[req-1].Item3.Replace(' ','_').Replace("\"","")),facts[r.Item2-1].Item3.Replace(' ', '_').Replace("\"", ""))))
+            {
+                string confRes;
+                res +=
+                    $"(defrule {string.Join("-", rule.Item1)}-{rule.Item2}" + "\r\n" +
+                    "(declare(salience 40))" + "\r\n" +
+                    string.Join("\r\n", rule.Item1.Select((req, i) => $"(element(param {req}) (confidence ?c{i}))")) +
+                    "\r\n" + "=>" + "\r\n" + $"(assert(element(param {rule.Item2}) (confidence ";
+
+                if (rule.Item1.Count() > 1)
+                    res += confRes = $"(/ (+ {string.Join(" ", rule.Item1.Select((req, i) => $"?c{i}")) }) {rule.Item1.Count()})";
+                else
+                    res += confRes = $"?c0";
+                res += ") ))" + "\r\n"+
+                $"(assert(appendmessagehalt (str-cat {string.Join(" \" + \" ",rule.Item1.Select((req, i) =>$"\"{req} (\" ?c{i} \")\""))} \" -> {rule.Item2} (\" {confRes} )))" + "\r\n" +
+                    ")\r\n\r\n";
+            }
+
+            return res;
+        }
+    }
+}
