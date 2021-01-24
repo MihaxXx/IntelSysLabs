3c1cea8dc50037e75ef3599fd565c58bc79e4f0a
Mikhail <mikes22@yandex.ru>
2021-01-25 01:34:25 +0300

    Upload Lab10

---
+++ IntelSysLab10/NeuralNetwork1/ImageGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork1
+{
+    public enum FigureType : byte { Close = 65, Like, Minus, Pause, Play, Plus, Repeat, Rewindbackward, RewindForward, Undef };
+
+    public class GenerateImage
+    {
+        /// <summary>
+        /// Бинарное представление образа
+        /// </summary>
+        public bool[,] img = new bool[200, 200];
+
+        /// <summary>
+        /// Текущая сгенерированная фигура
+        /// </summary>
+        public FigureType current_Letter = FigureType.Undef;
+
+        /// <summary>
+        /// Количество классов генерируемых фигур (4 - максимум)
+        /// </summary>
+        public int figure_count { get; set; } = 5;
+
+
+
+        /// <summary>
+        /// Очистка образа
+        /// </summary>
+        public void ClearImage()
+        {
+            for (int i = 0; i < 200; ++i)
+                for (int j = 0; j < 200; ++j)
+                    img[i, j] = false;
+        }
+
+        public Sample Generate(Bitmap image, char Letter = (char)90)
+        {
+            current_Letter = (FigureType)Letter;
+            double[] input = new double[21];
+
+
+            Bitmap a = AForge.Imaging.Image.Clone( new Bitmap(image), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            var uProcessed = AForge.Imaging.UnmanagedImage.FromManagedImage(a);
+
+            AForge.Imaging.Filters.Invert InvertFilter = new AForge.Imaging.Filters.Invert();
+            InvertFilter.ApplyInPlace(uProcessed);
+
+            AForge.Imaging.BlobCounterBase bc = new AForge.Imaging.BlobCounter();
+            bc.FilterBlobs = true;
+            bc.MinWidth = 2;
+            bc.MinHeight = 2;
+
+            bc.ProcessImage(uProcessed);
+            Rectangle[] rects = bc.GetObjectsRectangles();
+
+            for(int i = 0; i < rects.Length; i++)
+            {
+                input[i] = (double)rects[i].Width / (double)rects[i].Height;
+            }
+
+            input[20] = rects.Length;
+
+            return new Sample(input, figure_count, current_Letter);
+        }
+
+
+        /// <summary>
+        /// Возвращает битовое изображение для вывода образа
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap genBitmap()
+        {
+            Bitmap DrawArea = new Bitmap(200, 200);
+            for (int i = 0; i < 200; ++i)
+                for (int j = 0; j < 200; ++j)
+                    if (img[i, j])
+                        DrawArea.SetPixel(i, j, Color.Black);
+            return DrawArea;
+        }
+    }
+
+}
---
+++ IntelSysLab10/NeuralNetwork1/TLGBotik.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Drawing;
+
+namespace NeuralNetwork1
+{
+    class TLGBotik
+    {
+        public Telegram.Bot.TelegramBotClient botik = null;
+
+        AIMLBotik bott;
+
+        private UpdateTLGMessages formUpdater;
+
+        private BaseNetwork perseptron = null;
+
+        public TLGBotik(BaseNetwork net,  UpdateTLGMessages updater)
+        { 
+            var botKey = System.IO.File.ReadAllText("botkey.txt");
+            botik = new Telegram.Bot.TelegramBotClient(botKey);
+            botik.OnMessage += Botik_OnMessageAsync;
+            formUpdater = updater;
+            bott = new AIMLBotik();
+            perseptron = net;
+        }
+
+        public void SetNet(BaseNetwork net)
+        {
+            perseptron = net;
+            formUpdater("Net updated!");
+        }
+
+        private void Botik_OnMessageAsync(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            //  Тут очень простое дело - банально отправляем назад сообщения
+            var message = e.Message;
+            formUpdater("Тип сообщения : " + message.Type.ToString());
+
+            //  Получение файла (картинки)
+            if (message.Type == Telegram.Bot.Types.Enums.MessageType.Photo)
+            {
+                formUpdater("Picture loadining started");
+                var photoId = message.Photo.Last().FileId;
+                File fl = botik.GetFileAsync(photoId).Result;
+
+                var img = System.Drawing.Image.FromStream(botik.DownloadFileAsync(fl.FilePath).Result);
+                
+                System.Drawing.Bitmap bm = new System.Drawing.Bitmap(img);
+
+                //  Масштабируем aforge
+                AForge.Imaging.Filters.ResizeBilinear scaleFilter = new AForge.Imaging.Filters.ResizeBilinear(200,200);
+                var uProcessed = scaleFilter.Apply(AForge.Imaging.UnmanagedImage.FromManagedImage(bm));
+
+                var bb = uProcessed.ToManagedImage();
+
+                Camera processor = new Camera();
+                GenerateImage generator = new GenerateImage();
+
+                processor.ProcessImage(bb);
+
+                ////Сохрание обработанного образа
+                //int cnt = new System.IO.DirectoryInfo("D:\\МЕХМАТ\\course4_1\\ИС\\БОТ\\Debug").GetFiles().Length;
+
+                //Bitmap b = new Bitmap(processor.number.Width, processor.number.Height);
+                //b = processor.number;
+                //cnt++;
+                //string s = "D:\\МЕХМАТ\\course4_1\\ИС\\БОТ\\Debug\\" + cnt.ToString()+ ".bmp";
+                //b.Save(s);
+                ////
+                int symbolsCount = 9;
+
+                Sample sample = generator.Generate(processor.number);
+
+                switch (perseptron.Predict(sample))
+                {
+                    case FigureType.Close: botik.SendTextMessageAsync(message.Chat.Id, "Close");break;
+                    case FigureType.Like: botik.SendTextMessageAsync(message.Chat.Id, "Like"); break;
+                    case FigureType.Minus: botik.SendTextMessageAsync(message.Chat.Id, "Minus"); break;
+                    case FigureType.Pause: botik.SendTextMessageAsync(message.Chat.Id, "Pause"); break;
+                    case FigureType.Play: botik.SendTextMessageAsync(message.Chat.Id, "Play"); break;
+                    case FigureType.Plus: botik.SendTextMessageAsync(message.Chat.Id, "Plus"); break;
+                    case FigureType.Repeat: botik.SendTextMessageAsync(message.Chat.Id, "Repeat"); break;
+                    case FigureType.Rewindbackward: botik.SendTextMessageAsync(message.Chat.Id, "Rewindbackward"); break;
+                    case FigureType.RewindForward: botik.SendTextMessageAsync(message.Chat.Id, "RewindForward"); break;
+                    case FigureType.Undef: botik.SendTextMessageAsync(message.Chat.Id, "Undef"); break;
+                }
+
+                formUpdater("Picture recognized!");
+                return;
+            }
+
+            if (message == null || message.Type != Telegram.Bot.Types.Enums.MessageType.Text) return;
+
+            botik.SendTextMessageAsync(message.Chat.Id, bott.Talk(message.Text));
+            formUpdater(message.Text);
+            return;
+        }
+
+        private double[] imgToData(AForge.Imaging.UnmanagedImage img)
+        {
+            double[] res = new double[img.Width * img.Height];
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    //GetBrightness Возвращает значение освещенности (оттенок-насыщенность-освещенность (HSL)) для данной структуры
+                    res[i * img.Width + j] = img.GetPixel(i, j).GetBrightness(); // maybe threshold
+                }
+            }
+            return res;
+        }
+
+        public bool Act()
+        {
+            try
+            {
+                botik.StartReceiving();
+            }
+            catch(Exception e) { 
+                return false;
+            }
+            return true;
+        }
+
+        public void Stop()
+        {
+            botik.StopReceiving();
+        }
+
+    }
+}
